173d355e58595e17f6fd80fb80dab9802c7ecafa
Bharat Singh <bharatsing@bharatsing-macbookpro.roam.corp.google.com>
2020-05-27 17:22:02 -0700

    Add 503 Solution

--- 503.java
+++ 503.java
@@ -1,43 +1,18 @@
 class Solution {
-    // Binary Seacrh: O(n) * O( log(max -min) )
-    
-    public int shipWithinDays(int[] weights, int D) {
-        int min = 0;
-        int max = 0;
+    public int[] nextGreaterElements(int[] nums) {
+        int[] ret = new int[nums.length];
+        Arrays.fill(ret, -1);
         
-        for (int w : weights) {
-            min = Math.max(w, min);
-            max += w;
-        }
-        
-        // Binary Search for solution between {@code min} and {@code max}
-        int retVal = 0;
-        while(min <= max) { // O( log(max - min) )
-            int mid = (min + max) / 2;
-            int daysRequired = numberOfDaysNeededWithCapacity(weights, mid); // O(n)
-            
-            if (daysRequired > D) {
-                min = mid + 1;
-            } else {
-                retVal = mid;
-                max = mid - 1;
+        Stack<Integer> st = new Stack<>();
+        for (int x = 0; x < 2 * nums.length; x++) { // O(n) + O(n) = O(n)
+            while(!st.isEmpty() && nums[st.peek()] < nums[x % nums.length]) {
+                ret[st.pop()] = nums[x % nums.length];
             }
             
-        }
-        return retVal;
-    }
-    
-    private int numberOfDaysNeededWithCapacity(int[] weights, int capacity) {
-        int daysRequired = 1;
-        int current = 0;
-        
-        for (int w : weights) {
-            current += w;
-            if (current > capacity) {
-                daysRequired++;
-                current = w;
+            if (x < nums.length) {
+                st.push(x);
             }
         }
-        return daysRequired;
+        return ret;
     }
 }
